529313046ddacbf9f5f39da01c02303021e2d1fc
Alexander Sobolev <4nonym0us@xakep.ru>
2017-11-16 23:42:31 +0200

    [wip] Porting commands to utilize `CommandsNext` features

---
+++ TkokDiscordBot/Core/CommandsNext/PingCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using TkokDiscordBot.Core.Commands.Abstractions;
+using TkokDiscordBot.Core.Commands.Dto;
+
+namespace TkokDiscordBot.Core.CommandsNext
+{
+    internal class PingCommand : ICommandNext
+    {
+        [Command("ping")]
+        public async Task Ping(CommandContext context)
+        {
+            await context.RespondAsync($"!pong. WS Latency: {context.Client.Ping} ms.");
+        }
+
+        public CommandInfo GetUsage()
+        {
+            return null;
+        }
+    }
+}
