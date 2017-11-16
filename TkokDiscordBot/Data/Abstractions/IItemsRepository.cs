03fd6982522c89353d8f5e20b854c6556302264e
Alexander Sobolev <4nonym0us@xakep.ru>
2017-11-16 22:21:36 +0200

    Update GetItemByNameCommand to try to find for the exact match first.

--- TkokDiscordBot/Data/Abstractions/IItemsRepository.cs
+++ TkokDiscordBot/Data/Abstractions/IItemsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TkokDiscordBot.Entities;
 using TkokDiscordBot.Enums;
@@ -11,5 +12,6 @@ namespace TkokDiscordBot.Data.Abstractions
         Task<Item> Get(string name);
         Task<IEnumerable<Item>> Search(string name = null, string slot = null, string type = null, string quality = null, int? level = null, string boss = null);
         Task<IEnumerable<Item>> FullTextSearch(string filter, int? level, TkokClass @class);
+        Task<Item> FirstOrDefault(Func<Item, bool> predicate);
     }
 }
