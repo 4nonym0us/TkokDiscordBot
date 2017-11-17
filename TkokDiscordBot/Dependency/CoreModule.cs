e9b1588f2d48d1a3ab6714e309566fb415ea4c8b
Alexander Sobolev <4nonym0us@xakep.ru>
2017-11-17 09:08:06 +0200

    Refactor Autofac DI registration.

---
+++ TkokDiscordBot/Dependency/CoreModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Config.Net;
+using TkokDiscordBot.Configuration;
+using TkokDiscordBot.Core;
+using TkokDiscordBot.Data;
+using TkokDiscordBot.Data.Abstractions;
+using TkokDiscordBot.EntGaming;
+
+namespace TkokDiscordBot.Dependency
+{
+    internal class CoreModule :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PasteBinItemsLoader>().As<IItemsLoader>().InstancePerDependency(); //PasteBinItemsLoader|LocalFileItemsLoader
+            builder.RegisterType<ItemsStore>().As<IItemsStore>().SingleInstance();
+            builder.RegisterType<ItemsRepository>().As<IItemsRepository>().InstancePerDependency();
+            builder.RegisterType<EntClient>().AsSelf().SingleInstance();
+            builder.RegisterType<Bot>().AsSelf().SingleInstance();
+
+            var settings = new ConfigurationBuilder<ISettings>()
+                .UseAppConfig()
+                .UseJsonFile("user-secrets.json")
+                .Build();
+
+            builder.Register(c => settings).As<ISettings>().SingleInstance();
+        }
+    }
+}
