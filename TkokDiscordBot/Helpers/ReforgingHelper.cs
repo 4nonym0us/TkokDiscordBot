b74f67cbafd8bf51b7d999ff9078920a1cbef5bd
4nonym0us <shad0vvmind@gmail.com>
2024-08-23 16:36:47 +0300

    Fixed item reforging calculation to match wc3 algorithm. Changed IconUrl.

---
+++ TkokDiscordBot/Helpers/ReforgingHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using TkokDiscordBot.Entities;
+
+namespace TkokDiscordBot.Helpers
+{
+    internal class ReforgingHelper
+    {
+        private const double ReforgingMultiplier = 0.03334;
+        private static readonly string[] PropertiesToTruncateOnReforge =
+        {
+            "Power",
+            "Agility",
+            "Energy",
+            "Damage",
+            "Armor",
+            "Move speed",
+            "Life",
+            "Mana"
+        };
+
+        internal static void ReforgeProperties(Item item, short reforgeLevel)
+        {
+            foreach (var key in item.Properties.Keys.ToList())
+            {
+                var shouldBeTruncated = PropertiesToTruncateOnReforge.Any(x => x.Equals(key, StringComparison.OrdinalIgnoreCase));
+                var reforgedValue = Math.Round(item.Properties[key] * (1 + reforgeLevel * ReforgingMultiplier), 2);
+
+                item.Properties[key] = shouldBeTruncated ? Math.Truncate(reforgedValue) : reforgedValue;
+            }
+        }
+    }
+}
