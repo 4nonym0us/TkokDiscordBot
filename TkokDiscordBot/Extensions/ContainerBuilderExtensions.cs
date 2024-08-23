cbabce553fa98a560b4772c4c9ab5a83e4bf18d8
4nonym0us <shad0vvmind@gmail.com>
2024-08-23 16:36:50 +0300

    Refactor Autofac DI registration.

--- TkokDiscordBot/Extensions/ContainerBuilderExtensions.cs
+++ TkokDiscordBot/Extensions/ContainerBuilderExtensions.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
 using Autofac;
-using TkokDiscordBot.Core.Commands.Abstractions;
 
 namespace TkokDiscordBot.Extensions
 {
     public static class ContainerBuilderExtensions
     {
-        public static void RegisterCommands(this ContainerBuilder builder, Assembly assembly)
+        public static void RegisterCommands<TType>(this ContainerBuilder builder)
         {
-            var cmdInterface = typeof(IBotCommand);
+            var cmdInterface = typeof(TType);
+            var assembly = cmdInterface.Assembly;
             var types = assembly.GetTypes()
                 .Where(type => type.IsClass && !type.IsAbstract && cmdInterface.IsAssignableFrom(type))
                 .ToList();
 
             foreach (var type in types)
             {
-                builder.RegisterType(type).As<IBotCommand>().PreserveExistingDefaults();
+                builder.RegisterType(type).As<TType>().PreserveExistingDefaults();
             }
 
-            Console.WriteLine($"Loaded {types.Count} commands.");
+            Console.WriteLine($"Dynamically loaded {types.Count} {cmdInterface.Name} commands.");
         }
     }
 }
